4171676ee016417979b494ead91ab40169c43079
blurryface2809 <grazimashado@gmail.com>
2022-08-29 15:06:11 -0300

    exercicio2

---
+++ exercicio2/script.js
@@ -0,0 +1,12 @@
+let pergunta1 = (prompt("Você tem medo de fantasmas?"))
+let pergunta2 = (prompt("Você está sozinho agora?"))
+let pergunta3 = (prompt("Tem certeza?"))
+
+//console.log (letpergunta1, pergunta1)
+//console.log (letpergunta2, pergunta2)
+//console.log (letpergunta3, pergunta3)
+
+console.log(`Você tem medo de fantasma? ${pergunta1}`)
+console.log(`Você está sozinho agora? ${pergunta2}`)
+console.log(`Tem certeza? ${pergunta3}`)
+
